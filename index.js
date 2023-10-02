4d4df3505f85d70650fa766263ce28c61433fc21
michelle <junemichelle20@gmail.com>
2023-10-02 23:35:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => {
+      if (!resp.ok) {
+        error("response not ok");
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      renderBooks(data);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const bookList = document.getElementById("book-list");
+  books.forEach((book) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = book.name;
+    bookList.appendChild(listItem);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks(json());
 });
